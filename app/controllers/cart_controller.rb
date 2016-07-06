b6c06271bb481b4bb844f893879074eed084df1b
theochen <theochens@gmail.com>
2016-07-06 20:29:57 +0800

    authenticate_user

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,11 +1,7 @@
 class CartController < ApplicationController
   
   before_action :authenticate_user!
-  
-  # id is the product id via 'Add to cart' & routes.
-  # We're creating a cart session hash with key (product id) and value (quantity 1 or +1). This creates ["1", 1], ["2", 1], ["3", 2] etc.
-  # cart is defined at the Application Controller.
-  
+ 
   def add
     id = params[:id] 
     cart[id] ? cart[id] = cart[id] + 1 : cart[id] = 1 
