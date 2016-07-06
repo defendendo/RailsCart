e57e8de0234ab1d8033becafa49e7d121d8fb3d4
theochen <theochens@gmail.com>
2016-07-06 19:33:48 +0800

    add cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  #Makes cart available in views.
+  helper_method :cart
+  
+  # Ensure cart session hash exists, if not, it creates a new one with its Value to a blank hash.
+  
+  def cart  
+    session[:cart] ||= {}
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get '/cart' => 'cart#index'
+  get '/cart/clear' => 'cart#clearCart'
+  get '/cart/:id' => 'cart#add'
+
   devise_for :users
   root 'products#index'
 
