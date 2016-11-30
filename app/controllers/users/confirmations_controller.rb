3d4e7b9b93dc80c4ba60a63da431e9fa24d17e7b
Theo <theochens@gmail.com>
2016-11-30 12:16:31 +0800

    重寄註冊信2

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -31,6 +31,8 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     end
   end
 
+  protected
+  
   def after_resending_confirmation_instructions_path_for(resource_name)
     root_path
   end
